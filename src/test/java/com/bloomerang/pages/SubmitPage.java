626574908993622f33a38d270899a35a82fc28a2
Royal <merweska35@gmail.com>
2022-02-07 00:03:20 -0500

    project

---
+++ src/test/java/com/bloomerang/pages/SubmitPage.java
@@ -0,0 +1,115 @@
+package com.bloomerang.pages;
+
+import com.bloomerang.utility.ConfigReader;
+import com.bloomerang.utility.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+public class SubmitPage {
+
+    @FindBy(id = "first-name")
+    private WebElement firstnameElm;
+    @FindBy(id = "last-name")
+    private WebElement lastnameElm;
+    @FindBy(id = "email-address")
+    private WebElement emailElm;
+    @FindBy(id = "phone-number")
+    private WebElement phone;
+    @FindBy(id = "country")
+    private WebElement country;
+    @FindBy(id = "street-address")
+    private WebElement address;
+    @FindBy(id = "city")
+    private WebElement city;
+    @FindBy(id = "state")
+    private WebElement state;
+    @FindBy(id = "zip-code")
+    private WebElement zipcode;
+    @FindBy(id = "volunteer-date")
+    private WebElement volunteerDate;
+    @FindBy(id = "comment")
+    private WebElement comment;
+    @FindBy(xpath = "//input[@value='Submit']")
+    private WebElement submit;
+    @FindBy(xpath = "//h2[normalize-space()='Thank you for volunteering!']")
+    private WebElement message;
+
+    @FindBy(xpath = "//label[normalize-space()='This field is required.']")
+    private WebElement errorMessage;
+
+    public SubmitPage() {
+
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+    public void goTo() {
+
+        Driver.getDriver().get(ConfigReader.read("url"));
+
+    }
+
+    public void setFirstname(String name) {
+        firstnameElm.sendKeys(name);
+
+
+    }
+
+    public void lastname(String lastName) {
+        lastnameElm.sendKeys(lastName);
+    }
+
+    public void setEmail(String email) {
+        emailElm.sendKeys(email);
+    }
+
+    public void setPhone(String phoneNumber) {
+        phone.sendKeys(phoneNumber);
+    }
+
+    public void setCountry(String stateName) {
+        state.sendKeys(stateName);
+
+    }
+
+    public void streetAddress(String fullAddress) {
+        address.sendKeys(fullAddress);
+    }
+
+    public void setCity(String cityName) {
+        city.sendKeys(cityName);
+    }
+
+    public void state(String stateName) {
+        Select select = new Select(state);
+        select.deselectByValue(stateName);
+    }
+
+    public void setZipcode(String zipCodes) {
+        zipcode.sendKeys(zipCodes);
+    }
+
+    public void setVolunteerDate(String date) {
+        volunteerDate.sendKeys(date);
+    }
+
+    public void setComment() {
+        comment.sendKeys("Thank you");
+    }
+
+    public void setSubmitButton() {
+        submit.submit();
+    }
+
+    public String getMessage() {
+        return message.getText();
+    }
+    public String getErrorMessage() {
+        return errorMessage.getText();
+    }
+}
+
+
+
---
+++ src/test/java/com/bloomerang/steps/StepDef.java
@@ -0,0 +1,99 @@
+package com.bloomerang.steps;
+
+
+import com.bloomerang.pages.SubmitPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class StepDef {
+
+    SubmitPage submitPage = new SubmitPage();
+    @Given("The user is on the landing page to fill form")
+    public void the_user_is_on_the_landing_page_to_fill_form() {
+        submitPage.goTo();
+
+    }
+
+    @Given("I enter my first name {string}")
+    public void i_enter_my_first_name(String name) {
+        submitPage.setFirstname(name);
+    }
+
+    @Given("I enter my last name {string}")
+    public void i_enter_my_last_name(String lastName) {
+        submitPage.lastname(lastName);
+    }
+
+    @Given("I enter my email address {string}")
+    public void i_enter_my_email_address(String email) {
+        submitPage.setEmail(email);
+    }
+
+    @Given("I enter my phone number {string}")
+    public void i_enter_a_phone_number(String phone) {
+        submitPage.setPhone(phone);
+    }
+
+    @Given("I re-enter my phone number without country code {string}")
+    public void i_re_enter_my_phone_number_without_country_code(String phoneNoCode) {
+        submitPage.setPhone(phoneNoCode);
+    }
+
+    @Given("I enter my city name {string}")
+    public void i_did_not_touch_country_name(String city ) {
+        submitPage.setCity(city);
+    }
+
+    @Given("I enter my home address {string}")
+    public void i_enter_my_home_address(String address) {
+        submitPage.streetAddress(address);
+    }
+
+    @Given("I enter my state in USA {string}")
+    public void i_enter_my_state_in_usa(String state) {
+        submitPage.setCountry(state);
+    }
+
+    @Given("I enter zip code of my home address {string}")
+    public void i_enter_zip_code_of_my_home_address(String zip) {
+        submitPage.setZipcode(zip);
+    }
+
+    @Given("I choose date {string}")
+    public void i_choose_date(String date) {
+        submitPage.setVolunteerDate(date);
+    }
+
+    @Given("I write comments")
+    public void i_write_comments_about_volunteering_date() {
+        submitPage.setComment();
+    }
+
+    @When("I enter the Submit button")
+    public void i_enter_the_submit_button() {
+        submitPage.setSubmitButton();
+    }
+
+
+    @When("I press the Submit button")
+    public void i_press_the_submit_button() {
+        submitPage.setSubmitButton();
+    }
+
+    @Then("I am able to see {string} in the page.")
+    public void iAmAbleToSeeInThePage(String message) {
+        String actualResult= submitPage.getMessage();
+        Assert.assertEquals(message,actualResult);
+    }
+
+    @Then("I am able to see {string} error message in the page.")
+    public void iAmAbleToSeeErrorMessageInThePage(String message ) {
+        String actualResult = submitPage.getErrorMessage();
+        Assert.assertEquals(message,actualResult);
+    }
+}
+
+
+
